27f2646a9527c863b457b133be13c71563b8cb73
leilimin <461083868@qq.com>
2022-08-30 13:58:45 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    using namespace std;// C++的新标准,预处理中的头文件如果为不加.h后缀的形式就可以使用这种"声明名称空间的方式"
+    cout<<"Hello World";// 格式符重载(eg: &既可以表示为取地址,也可以表示为位AND操作)
+    cout<<endl;         // 光标换行
+    cin.get();          // 为了不让控制行闪退
 }
