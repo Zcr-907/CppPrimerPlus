15a2ac2538e6e3fb5d8a8d456a657e53cf59f3af
leilimin <461083868@qq.com>
2022-09-02 22:44:41 +0800

    upgrade: CMake

---
+++ chapter_2/S01_BitOffset.cpp
@@ -0,0 +1,14 @@
+// Created by Zcr on 2022/9/2.
+#include <iostream>
+
+void BitOffset() {
+    using namespace std; // 设置名称空间
+    // 测试证明: << 左移运算符,会讲所有的高位依次剔除。例如10....111 <===> 0.....1110
+    cout << "[有符号负数左移]: " << (0xBFFFFFFF << 1) << endl;
+
+    // 测试证明: 无符号数与有符号数一起操作时,先将有符号数转为无符号数进行同一操作
+    unsigned int uZero = 0;
+    cout << "[uZero > -1]: " << (uZero > -1) << endl;
+}
+
+
