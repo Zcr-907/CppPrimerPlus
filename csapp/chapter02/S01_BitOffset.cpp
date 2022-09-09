127ef7bb23969ff36baab7a781b2a9caeaef5f21
leilimin <461083868@qq.com>
2022-09-09 13:57:16 +0800

    format(csapp)

--- csapp/chapter02/S01_BitOffset.cpp
+++ csapp/chapter02/S01_BitOffset.cpp
@@ -8,6 +8,13 @@ void lab01() {
     // 测试证明: 无符号数与有符号数一起操作时,先将有符号数转为无符号数进行同一操作
     unsigned int uZero = 0;
     cout << "[uZero > -1]: " << (uZero > -1) << endl;
+
+    // 测试证明: 无符号数与有符号数一起操作时,先将有符号数转为无符号数进行同一操作
+    //          以下会陷入死循环
+    unsigned int i = 0;
+    for (int j = 0; j >= i; j--) {
+        cout << j << endl;
+    }
 }
 
 
