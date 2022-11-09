339febaab096ba63dd46d0d8846b3a8b2bd4c56e
leilimin <461083868@qq.com>
2022-11-09 23:33:23 +0800

    feat: cpp
    1.pointer

--- CppPrimerPlus/chapter04/pointer.cpp
+++ CppPrimerPlus/chapter04/pointer.cpp
@@ -41,6 +41,11 @@ void pointer_func() {
     cout << "After Add 1 p_arr_int[2]: " << p_arr_int[2] << endl;
     // 释放数组内存时需要添加[]
     delete[] p_arr_int;
+
+    int int_arr[] = {1, 2, 3};
+    int *p_arr_1 = int_arr;     // 数组名int_arr为数组首元素地址
+    p_arr_1 = p_arr_1 + 1;      // p_arr_1 = &int_arr[0] + sizeof(int)
+    int (*p_arr)[3] = &int_arr; // &int_arr虽然在数字表示上和int_arr一致,但是p_arr+1 = &int_arr[0] + sizeof(int_arr)
     cout << "-------> pointer_func <-------" << endl;
 
 }
