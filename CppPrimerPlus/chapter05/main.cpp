6476b83f001fc8d96b4fb5e5a4a4c098148d6b9c
leilimin <461083868@qq.com>
2022-11-12 22:24:48 +0800

    feat: cpp
    1.loop

---
+++ CppPrimerPlus/chapter05/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int int_arr[] = {1, 2, 3};
+    int i, j, temp;
+
+    /**
+     * 逗号表达式
+     * int i, j, temp; => 此处逗号并不意味着逗号运算符,而是意味着声明分割
+     * i++, j--        => 该处逗号意味着逗号运算符,且每个逗号都为一个顺序点(顺序点: 程序保证在该顺序点之前的所有副作用都已完成)
+     */
+    for (i = 0, j = (sizeof(int_arr) / sizeof(int)) - 1; j < i; i++, j--) {
+        temp = int_arr[i];
+        int_arr[i] = int_arr[j];
+        int_arr[j] = temp;
+
+
+    }
+
+    for (int k = 0; k < (sizeof(int_arr) / sizeof(int)); ++k) {
+        cout << "第" << k << "个数: " << int_arr[k];
+        // ios_base::boolalpha => 将表达式的值按照Boolean类型输出
+        cout.setf(ios_base::boolalpha);
+        cout << "\t是否为偶数: " << (int_arr[k] % 2 == 0) << endl;
+    }
+
+    /**
+     * 再次验证:
+     * true: 字符指针共用的是相同字符串字面量       => p_char是 &p_char中存储的数据也就是"ABC"的地址
+     * false: 数组会开辟一个空间存储该字面量的副本   => char_arr = &char_arr[0] 是字符数组的首元素地址
+     */
+    char *p_char = "ABC";
+    char char_arr[] = "ABC";
+    cout << R"+*(p_char=="ABC": )+*" << (p_char == "ABC") << endl;
+    cout << R"+*(char_arr=="ABC": )+*" << (char_arr == "ABC") << endl;
+
+}
