4f26c8ef98adf96c6ad6b6578e0bbad94910f218
leilimin <461083868@qq.com>
2022-11-13 23:07:09 +0800

    feat: cpp
    1.eof

--- CppPrimerPlus/chapter05/main.cpp
+++ CppPrimerPlus/chapter05/main.cpp
@@ -31,19 +31,29 @@ int main() {
      * true: 字符指针共用的是相同字符串字面量       => p_char是 &p_char中存储的数据也就是"ABC"的地址
      * false: 数组会开辟一个空间存储该字面量的副本   => char_arr = &char_arr[0] 是字符数组的首元素地址
      */
-    char *p_char = "ABC";
+    const char *p_char = "ABC";
     char char_arr[] = "ABC";
     cout << R"+*(p_char=="ABC": )+*" << (p_char == "ABC") << endl;
     cout << R"+*(char_arr=="ABC": )+*" << (char_arr == "ABC") << endl;
 
     // 基于范围的for循环
-    for (char element: char_arr) {  // 类似java增强for循环,但是不能修改成员的值
+    for (char element : char_arr) {  // 类似java增强for循环,但是不能修改成员的值
         cout << "element: " << element << endl;
     }
-    for (char &element: char_arr) { // &这种方式拓展了该类型循环类可以修改成员的方式
+    for (char &element : char_arr) { // &这种方式拓展了该类型循环类可以修改成员的方式
         element += 1;
     }
     cout << "every char add one value:" << char_arr << endl;
 
+
+    /**
+     * cin.get(ch): 将缓存区读到的字符存入ch,并且返回cin实例,如果需要将cin表示为bool的地方
+     *              如果遇到了EOF,则该cin表示为false,否则为true
+     */
+    char ch;
+    while (cin.get(ch)) {
+        cout << ch;
+    }
+    cout << "EOF,Exit" << endl;
 
 }
