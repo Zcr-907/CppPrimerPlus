d14b3fe4eca06ac91d13b49d90600fb39f4fee6a
leilimin <461083868@qq.com>
2022-11-26 19:58:01 +0800

    feat: cpp
    1.class

--- CppPrimerPlus/chapter10/Stock.cpp
+++ CppPrimerPlus/chapter10/Stock.cpp
@@ -1,3 +1,54 @@
 #include "Stock.h"
+#include <iostream>
 
-Stock s1;
+using namespace std;
+
+void Stock::acquire(const string &co, long n, double pr) {
+    company = co;
+    if (n < 0) {
+        cout << "Number of shares can't be negative;"
+             << company << " shares set to 0.\n";
+        shares = 0;
+    } else {
+        shares = n;
+    }
+    share_val = pr;
+    set_tot();
+}
+
+void Stock::buy(long num, double price) {
+    if (num < 0) {
+        cout << " Number of shares purchased can't be negative."
+             << "Transaction is aborted.\n";
+    } else {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price) {
+    if (num < 0) {
+        cout << " Number of shares purchased can't be negative."
+             << "Transaction is aborted.\n";
+    } else if (num > shares) {
+        cout << "You Can't sell more than you have!"
+             << "Transaction is aborted.\n";
+    } else {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price) {
+    share_val = price;
+    set_tot();
+}
+
+//void Stock::show() {
+//    cout << "Company: " << company
+//         << " shares: " << shares << endl
+//         << " share price: " << share_val
+//         << " total Worth: " << total_val << endl;
+//}
--- CppPrimerPlus/chapter10/main.cpp
+++ CppPrimerPlus/chapter10/main.cpp
@@ -1,14 +1,27 @@
-#include <iostream>
+#include "Stock.h"
 
-class Human {
-private:
-    int age;
-    std::string name;
-public:
-    int height;
-    std::string beauty;
-};
 
-int main(){
+int main() {
+    cout << "-------> 类对象创建 <-------" << endl;
+
+    // 理解以下不同的使用方式差别
+    Stock sally;            // 为sally分配了内存空间(栈上),但是所指向的位置是未知的
+    sally.show();
+
+    auto *solly = new Stock;// 为solly分配了内存空间(堆上),分配的内存区块都进行了零值初始化
+    (*solly).show();
+    cout << "-------> 类对象创建 <-------" << endl;
+    cout << "-------> 类对象使用 <-------" << endl;
+    sally.acquire("NanoSmart", 20, 12.50);
+    sally.show();
+    sally.buy(15, 18.125);
+    sally.show();
+    sally.sell(400, 20.00);
+    sally.show();
+    sally.buy(300000, 40.125);
+    sally.show();
+    sally.sell(300000, 0.125);
+    sally.show();
+    cout << "-------> 类对象使用 <-------" << endl;
 
 }
