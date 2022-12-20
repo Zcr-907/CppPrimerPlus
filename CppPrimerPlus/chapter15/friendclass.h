7e50f73198eae5b0c359380fd0d7c816b10f2813
leilimin <461083868@qq.com>
2022-12-20 22:23:31 +0800

    format: cpp
    1.安全的类型转换

--- CppPrimerPlus/chapter15/friendclass.h
+++ CppPrimerPlus/chapter15/friendclass.h
@@ -24,5 +24,40 @@ public:
     }
 };
 
+/**
+ * RTTI
+ */
+class Grand {
+    int _num;
+public:
+    explicit Grand(int num = 0) : _num(num) {}
+
+    virtual void speak() const {
+        cout << "i am a Grand" << endl;
+    };
+};
+
+class Major : public Grand {
+public:
+    void speak() const override {
+        cout << "i am a Major" << endl;
+    };
+
+    virtual void say() const {
+        cout << "Major Say" << endl;
+    }
+};
+
+class Minor : public Major {
+public:
+    void speak() const override {
+        cout << "i am a Minor" << endl;
+    };
+
+    void say() const override {
+        cout << "Minor Say" << endl;
+    }
+};
+
 
 #endif //CPP_DEV_FRIENDCLASS_H
--- CppPrimerPlus/chapter15/main.cpp
+++ CppPrimerPlus/chapter15/main.cpp
@@ -1,8 +1,40 @@
 #include "friendclass.h"
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
 
 int main() {
     Tv tv("Lewis");
     Remote rt;
     rt.getTvName(tv);
+    cout << "------- RTTI -------" << endl;
+    Grand *pg = new Grand();
+    Grand *pmj = new Major();
+    Grand *pmi = new Minor();
+
+    Major *castPmj;
+    cout << "------- dynamic_cast -------" << endl;
+    if (castPmj = dynamic_cast<Major *>(pg)) {
+        castPmj->speak();
+        castPmj->say();
+    }
+    if (castPmj = dynamic_cast<Major *>(pmj)) {
+        castPmj->speak();
+        castPmj->say();
+    }
+    if (castPmj = dynamic_cast<Major *>(pmi)) {
+        castPmj->speak();
+        castPmj->say();
+    }
+    cout << "------- dynamic_cast -------" << endl;
+    cout << "------- typeid -------" << endl;
+    bool bmj = typeid(Major) == typeid(*pmj);
+    bool bg = typeid(Major) == typeid(*pg);
+    cout << "bmj:" << bmj << endl;
+    cout << "bg:" << bg << endl;
+
+    cout << "------- typeid -------" << endl;
+    cout << "------- RTTI -------" << endl;
 }
 
