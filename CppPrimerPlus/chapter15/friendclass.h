f858b55e718e6541ee5d5673e852b0e782ed0966
leilimin <461083868@qq.com>
2022-12-23 11:56:00 +0800

    format: cpp
    1.format

--- CppPrimerPlus/chapter15/friendclass.h
+++ CppPrimerPlus/chapter15/friendclass.h
@@ -5,33 +5,84 @@
 
 using namespace std;
 
+// =====================
+// 友元类: 如下例子中,Tv类中声明了Remote为Tv的友元类,那么后续在Remote的作用域内就可以访问Tv的私有元素
+// =====================
 class Tv {
-private:
-    string _name;
-public:
-    // 声明了一个友元类,该友元类可以直接访问Tv的私有元素
+    // 声明Remote为Tv的友元类
     friend class Remote;
 
-    Tv(string name) : _name(name) {}
+private:
+    string _name;
+
+public:
+
+    explicit Tv(string &name);
 
 };
 
 class Remote {
+private:
+    string _name;
 public:
-    void getTvName(const Tv &t) {
-        // 访问到了tv的私有原属
-        cout << t._name << endl;
-    }
+    explicit Remote(string &name);
+
+    void printTvName(const Tv &t) const;
+
 };
 
-/**
- * RTTI
- */
-class Grand {
-    int _num;
-public:
-    explicit Grand(int num = 0) : _num(num) {}
+inline Tv::Tv(string &name) : _name(name) {}
 
+inline Remote::Remote(string &name) : _name(name) {}
+
+// 由于Remote是Tv的友元类,所以Remote类的方法可以访问Tv实例的私有属性
+inline void Remote::printTvName(const Tv &t) const {
+    cout << _name << " Print: [" << t._name << "]" << endl;
+}
+
+// =====================
+// 友元方法: 如果说B类只有一个方法会使用到A类的私有属性,那么将整个B类声明为A类的友元类显得不那么必要,可以将B类的那个方法声明为A类的友元方法
+//          这样的话只有B类的那个指定的友元方法才可以访问A类的私有属性
+// 前置声明: 由于A和B在声明的时候需要知道互相的存在
+//          对于B而言由于方法声明只需要知道A类的存在。对于A而言,需要B的类声明(包括方法声明)。
+//          所以B的完整声明需要在A的完整声明之前,A的前置声明需要在B的完整声明之前,整个声明过程如下
+// =====================
+// 前置声明
+class Tv1;
+
+class Remote1 {
+public:
+    // updateTv1Name()在Tv1的声明中被声明为友元方法,所以该方法可以操作Tv1的私有属性
+    static void updateTv1Name(Tv1 &tv1, string &name);
+};
+
+class Tv1 {
+    // 声明Remote1::updateTv1Name()为Tv1的友元方法
+    friend void Remote1::updateTv1Name(Tv1 &tv1, string &name);
+
+private:
+    string _name;
+public:
+    explicit Tv1(string &name);
+
+    void toString() const;
+};
+
+inline Tv1::Tv1(string &name) : _name(name) {}
+
+inline void Tv1::toString() const {
+    cout << "Tv1: " << _name << endl;
+}
+
+inline void Remote1::updateTv1Name(Tv1 &tv1, string &name) {
+    tv1._name = name;
+}
+
+// =====================
+// RTTI: 运行时类型识别
+// =====================
+class Grand {
+public:
     virtual void speak() const {
         cout << "i am a Grand" << endl;
     };
@@ -59,39 +110,5 @@ public:
     }
 };
 
-// >>>>>>> 前置声明 >>>>>>>
-/**
- * 1.由于Tv1将Remote1::opsTv1()声明为友元方法,所以在Tv1声明之前应该至少存在Remote1::opsTv1()的声明
- * 2.Remote1::opsTv1()的声明至少需要存在Tv1类,所以在这之前前置声明了一个Tv1
- */
-class Tv1;
-
-class Remote1 {
-public:
-    void opsTv1(Tv1 &tv1, string name);
-};
-
-
-class Tv1 {
-    friend void Remote1::opsTv1(Tv1 &tv1, string name);
-
-private:
-    string _name;
-
-public:
-    Tv1(string &name);
-
-    void toString() const;
-};
-
-inline Tv1::Tv1(string &name) : _name(name) {}
-
-inline void Tv1::toString() const {
-    cout << _name << endl;
-}
-
-inline void Remote1::opsTv1(Tv1 &tv1, string name) {
-    tv1._name = name;
-}
 
 #endif //CPP_DEV_FRIENDCLASS_H
--- CppPrimerPlus/chapter15/main.cpp
+++ CppPrimerPlus/chapter15/main.cpp
@@ -1,57 +1,65 @@
 #include "friendclass.h"
 #include <iostream>
-#include <typeinfo>
+#include <cstdlib>
+#include <ctime>
 
+#define DYNAMIC_CAST_LOOP 5
 using namespace std;
 
+
+Grand *getOneGrand();
+
+
 int main() {
-    Tv tv("Lewis");
-    Remote rt;
-    rt.getTvName(tv);
-    cout << "------- RTTI -------" << endl;
-    Grand *pg = new Grand();
-    Grand *pmj = new Major();
-    Grand *pmi = new Minor();
+    cout << "------- 友元类 -------" << endl;
+    string publicPartFriendClass = "FriendClass";
+    string tv_1_name = publicPartFriendClass + "|tv_1";
+    string remote_1_name = publicPartFriendClass + "|remote_1";
 
-    Major *castPmj;
-    cout << "------- dynamic_cast -------" << endl;
-    if (castPmj = dynamic_cast<Major *>(pg)) {
-        castPmj->speak();
-        castPmj->say();
-    }
-    if (castPmj = dynamic_cast<Major *>(pmj)) {
-        castPmj->speak();
-        castPmj->say();
-    }
-    if (castPmj = dynamic_cast<Major *>(pmi)) {
-        castPmj->speak();
-        castPmj->say();
-    }
-    cout << "------- dynamic_cast -------" << endl;
-    cout << "------- typeid -------" << endl;
-    bool bmj = typeid(Major) == typeid(*pmj);
-    bool bg = typeid(Major) == typeid(*pg);
-    cout << "bmj:" << bmj << endl;
-    cout << "bg:" << bg << endl;
+    Tv tv_1(tv_1_name);
+    Remote remote_1(remote_1_name);
+    remote_1.printTvName(tv_1);
 
-    cout << "------- typeid -------" << endl;
-    cout << "------- RTTI -------" << endl;
+    cout << "------- 友元方法,前置声明 -------" << endl;
+    string publicPartFriendMethod = "FriendMethod";
+    string tv1_1_name = publicPartFriendMethod + "|tv1_1";
+    Tv1 tv1_1(tv1_1_name);
+    Remote1 remote1_1;
+    tv1_1.toString();
+    string updateName = publicPartFriendMethod + "|updateName";
+    remote1_1.updateTv1Name(tv1_1, updateName);
+    tv1_1.toString();
+
+    cout << "------- RTTI#dynamic_cast -------" << endl;
+    srand(time(nullptr));
+    int n = DYNAMIC_CAST_LOOP;
+    while (n-- > 0) {
+        Grand *pg = getOneGrand();
+        cout << ">dynamic_cast#loop#" << n << "<" << endl;
+        pg->speak();
+        // 如果pg为Major及其子类那么就会返回一个Major*的指针引用,否则就会返回一个空指针
+        if (dynamic_cast<Major *>(pg)) {
+            dynamic_cast<Major *>(pg)->say();
+        }
+        delete pg;
+    }
+
+    cout << "------- RTTI#typeid -------" << endl;
+    n = DYNAMIC_CAST_LOOP;
+    while (n-- > 0) {
+        Grand *pg = getOneGrand();
+        cout << ">typeid#loop#" << n << "<" << endl;
+        pg->speak();
+        // pg的真实类型必须为Major,才会判断为True
+        if (typeid(Major) == typeid(*pg)) {
+            dynamic_cast<Major *>(pg)->say();
+        }
+        delete pg;
+    }
 
     cout << "------- tryCatch -------" << endl;
     void tryCatch(int a, int b);
     tryCatch(1, 0);
-    cout << "------- tryCatch -------" << endl;
-
-    cout << "------- friend method -------" << endl;
-    string name = "tv1_1";
-    Tv1 tv1_1(name);
-    Remote1 r1_1;
-
-    tv1_1.toString();
-    string opsName = "ops_remote";
-    r1_1.opsTv1(tv1_1, opsName);
-    tv1_1.toString();
-    cout << "------- friend method -------" << endl;
 }
 
 void tryCatch(int a, int b) {
@@ -62,6 +70,21 @@ void tryCatch(int a, int b) {
         cout << a / b << endl;
     } catch (const char *message) {
         cout << message << endl;
+    }
+}
+
+// 随机生成Grand继承树上的对象
+Grand *getOneGrand() {
+    int random = rand() % 3;
+    switch (random) {
+        case 0:
+            return new Grand();
+        case 1:
+            return new Major();
+        case 2:
+            return new Minor();
+        default:
+            throw exception();
     }
 }
 
